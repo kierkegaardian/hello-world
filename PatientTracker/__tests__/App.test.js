44ec181ed6097d56c513330cd6cb0541769cdc60
William G. Smith <wgsmith@yandex.com>
2025-05-25 13:43:29 -0500

    Configure Jest transform ignore patterns

--- PatientTracker/__tests__/App.test.js
+++ PatientTracker/__tests__/App.test.js
@@ -4,6 +4,7 @@ import App from '../App';
 
 describe('App', () => {
   it('renders without crashing', () => {
-    renderer.create(<App />);
+    const tree = renderer.create(<App />);
+    tree.unmount();
   });
 });
